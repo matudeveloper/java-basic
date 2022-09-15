ec85513b57b556c43a962f3de380f7e444d9db6c
Matu developer <martin.laan@khk.ee>
2022-09-15 10:19:46 +0300

    updated java exercises

---
+++ week1/Ex5.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+
+        int secondsInYear = days * hours * minutes * seconds;
+
+        System.out.println("There are " + secondsInYear + " seconds in a year.");
+    }
+}
