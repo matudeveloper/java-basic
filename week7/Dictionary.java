1ce8bf6bbc1c283efe72e9379dcf87006f798f34
Matu developer <martin.laan@khk.ee>
2022-09-16 14:40:56 +0300

    7.5: Adding and Translating Words

---
+++ week7/Dictionary.java
@@ -0,0 +1,36 @@
+package week7;
+
+public class Dictionary {
+    HashMap<String, String> dictionary;
+
+    public Dictionary() {
+        this.dictionary = new HashMap<String, String>();
+
+    }
+
+    public void add(String word, String translation){
+        this.dictionary.put(word, translation);
+    }
+
+    public String translate(String word){
+        if(this.dictionary.containsKEy(word)){
+            return this.dictionary.get(word);
+        }
+        return null;
+    }
+
+    public int amountOfWords(){
+        return this.dictionary.size()
+    }
+
+    public ArrayList<String> translationList(){
+        ArrayList<String> transList = new ArrayList<String>();
+        for (String key: this.dictionary.keySet())
+        {
+            String value = this.dictionary.get(key);
+            String pair = key + " = " + value;
+            transList.add(pair);
+        }
+        return transList;
+    }
+}
